f6762d6c15be19a7a073b8d9de4af1e52f6fd23f
Maciej Wiatr <maciekwiatr17@gmail.com>
2019-03-18 15:39:29 +0100

    first commit

---
+++ data.py
@@ -0,0 +1,30 @@
+from fbchat.models import *
+
+colours = (ThreadColor.PUMPKIN, ThreadColor.BILOBA_FLOWER,
+           ThreadColor.BRILLIANT_ROSE, ThreadColor.LIGHT_CORAL,
+           ThreadColor.GOLDEN_POPPY, ThreadColor.VIKING,
+           ThreadColor.RADICAL_RED, ThreadColor.CAMEO,
+           ThreadColor.MEDIUM_SLATE_BLUE, ThreadColor.FREE_SPEECH_GREEN)
+
+emojis = ("❤️", "😂", "😯", "😍", "😂", "😱", "😮",
+          "👊🏼", "🐼", "💪🏿", "🎮", "🌊", "🍕", "🙀")
+
+anime_gifs = [
+              "https://i.imgur.com/C1kNFGM.gif",
+              "https://i.imgur.com/LbkbuDI.gif",
+              "https://i.imgur.com/DVn6B8G.gif",
+              "https://i.imgur.com/RWiXIW0.gif",
+              "https://i.imgur.com/nvpzLQ9.gif",
+              "https://i.imgur.com/HBQeiYN.gif",
+              "https://i.imgur.com/sfYF5kh.gif",
+              "https://i.imgur.com/qPaKyGU.gif",
+              "https://i.imgur.com/mNPiQgJ.gif",
+              "https://i.imgur.com/DLoTIgh.gif",
+              "https://i.imgur.com/5QdyHUx.gif",
+              "https://i.imgur.com/tgxEhUU.gif",
+              "https://i.imgur.com/s7MUhRa.gif",
+              "https://i.imgur.com/LeBzqbC.gif",
+              "https://i.imgur.com/DCdyaHd.gif"
+
+
+             ]
